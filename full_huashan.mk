8ac8df468990068ac0d2513d2f69a1a3f7e274de
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2013-10-20 22:38:20 +0200

    huashan: use common resources
    Change-Id: I8d7db01bc6c7d549d5ff0e07a171179ffe525034

--- full_huashan.mk
+++ full_huashan.mk
@@ -34,7 +34,7 @@ PRODUCT_PACKAGES := \
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Common Sony Resources
-$(call inherit-product, device/sony/common/resources-xhdpi.mk)
+$(call inherit-product, device/sony/common/resources.mk)
 
 # Inherit from (this) huashan device makefile
 $(call inherit-product, device/sony/huashan/huashan.mk)
