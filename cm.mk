9e50c79bdc530af39ba7b9dd3ac02e9c75f879f2
Brinly Taylor <uberlaggydarwin@gmail.com>
2014-05-15 10:32:01 +0930

    huashan: cleanup a makefile pointing to a no longer existing file.
    update the build fingerprint
    
    Change-Id: I445c6f86cd358f48aec5ad64a3ef25b31328f743

--- cm.mk
+++ cm.mk
@@ -1,8 +1,5 @@
 $(call inherit-product, device/sony/huashan/full_huashan.mk)
 
-# Inherit CM common GSM stuff.
-$(call inherit-product, vendor/cm/config/gsm.mk)
-
 # Enhanced NFC
 $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
@@ -10,7 +7,7 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
 # Build fingerprints
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C5303_1272-3352 BUILD_FINGERPRINT=Sony/C5303_1272-3352/C5303:4.1.2/12.0.A.2.254/Aff_nw:user/release-keys PRIVATE_BUILD_DESC="C5303-user 4.1.2 12.0.A.2.254 Aff_nw test-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=C5303 BUILD_FINGERPRINT=Sony/C5303/C5303:4.3/12.1.A.0.266/9f5_nw:user/release-keys PRIVATE_BUILD_DESC="C5303-user 4.3 JB-MR2-VISKAN-CAF-140119-1358 30 test-keys"
 
 PRODUCT_NAME := cm_huashan
 PRODUCT_DEVICE := huashan
