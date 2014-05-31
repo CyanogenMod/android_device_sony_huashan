d811f72e82c341711588e3a2c02f6dce3b0fc444
Adam Farden <adam@farden.cz>
2014-05-31 22:44:21 +0200

    Allow huashan to use own Adreno blobs
    Change-Id: Ie4535a88c022fc65704f76453c12f03ff1feaf64

--- huashan.mk
+++ huashan.mk
@@ -260,3 +260,7 @@ $(call inherit-product-if-exists, frameworks/native/build/phone-xhdpi-1024-dalvi
 
 # Include non-opensource parts/ proprietary files
 $(call inherit-product, vendor/sony/huashan/huashan-vendor.mk)
+
+# Include non-opensource parts/ proprietary files
+$(call inherit-product, vendor/sony/huashan/huashan-adreno-blobs.mk)
+
