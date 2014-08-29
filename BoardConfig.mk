42d0e17673f32688b7d53c0c8388db97e8614fb9
Karol Stumski <zenderteam@gmail.com>
2014-08-29 18:37:58 +0200

    replaced this shit

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,7 +27,7 @@ TARGET_OTA_ASSERT_DEVICE := C5302,C5303,C5306,huashan
 TARGET_SPECIFIC_HEADER_PATH += device/sony/huashan/include
 
 # Kernel properties
-TARGET_KERNEL_SOURCE := kernel/sony/msm8x60
+TARGET_KERNEL_SOURCE := kernel/sony/msm8960t
 TARGET_KERNEL_CONFIG := cm_viskan_huashan_defconfig
 
 # Platform
