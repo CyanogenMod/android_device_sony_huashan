6b4299292ea8d3fee386aebc4f1fe88e26ba79da
Giulio Cervera <giulio.cervera@cyanogenmod.org>
2014-11-02 15:11:57 +0100

    script: fix traditional sort
    Change-Id: I23fe22b74f9ac8cd6a09d932f1f4493850c6e7d2

--- extract-files.sh
+++ extract-files.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-export DEVICE=${PWD##*/}
+# Use tradition sort
+export LC_ALL=C
+
+FP=$(dirname $(readlink -f $0))
+export VENDOR=$(basename $(dirname $FP))
+export DEVICE=$(basename $FP)
 export BOARDCONFIGVENDOR=true
 
 ../common/extract-files.sh $@
