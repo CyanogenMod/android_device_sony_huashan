b0bc8f7a0f2b028176b230aa8e11e2a74819a932
Brinly Taylor <uberlaggydarwin@gmail.com>
2014-11-15 07:58:43 +1030

    WIP: huashan: basic bringup for L
    (need to fix sepolicies but will have a patch for that soon)
    add support for legacy mmap for blobs
    huashan: revert to old logging behavior by setting TARGET_USES_LOGD to false
    
    Change-Id: I82d2e5620a211a0bb0441037c0abea24b4cb784e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -18,9 +18,6 @@ include device/sony/common/BoardConfigCommon.mk
 # inherit from msm8960-common
 include device/sony/msm8960-common/BoardConfigCommon.mk
 
-# inherit from the proprietary version
--include vendor/sony/huashan/BoardConfigVendor.mk
-
 # Assert
 TARGET_OTA_ASSERT_DEVICE := C5302,C5303,C5306,huashan
 
@@ -36,12 +33,13 @@ TARGET_BOARD_PLATFORM := msm8960
 BOARD_VENDOR_PLATFORM := viskan
 
 # Architecture
-TARGET_CPU_VARIANT := krait
+
 
 # Flags
 TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
+TARGET_CPU_VARIANT := krait
 
 # Krait optimizations
 TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
@@ -52,10 +50,13 @@ TARGET_KRAIT_BIONIC_BBTHRESH  := 64
 TARGET_KRAIT_BIONIC_PLDSIZE   := 64
 
 # Kernel information
-BOARD_KERNEL_BASE     := 0x80200000
+BOARD_KERNEL_BASE := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE  := androidboot.hardware=qcom user_debug=31 androidboot.baseband=msm msm_rtb.filter=0x3F ehci-hcd.park=3 vmalloc=400M
-BOARD_MKBOOTIMG_ARGS  := --ramdisk_offset 0x02000000
+BOARD_MKBOOTIMG_ARGS  := --ramdisk_offset 0x0200000
+
+# Time
+BOARD_USES_QC_TIME_SERVICES := true
 
 # Dumpstate
 BOARD_LIB_DUMPSTATE := libdumpstate.sony
@@ -90,11 +91,11 @@ BOARD_HAVE_BLUETOOTH_QCOM := true
 BLUETOOTH_HCI_USE_MCT := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/huashan/bluetooth
 
+# Legacy RIL
+BOARD_HAS_RIL_LEGACY_PAP := true
+
 # Needed for blobs
 TARGET_RELEASE_CPPFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
-
-# Time
-BOARD_USES_QC_TIME_SERVICES := true
 
 # Vold
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
@@ -113,27 +114,15 @@ TARGET_USERIMAGES_USE_EXT4 := true
 
 # QCOM/CAF hardware
 BOARD_USES_QCOM_HARDWARE := true
-TARGET_QCOM_AUDIO_VARIANT := caf
-TARGET_QCOM_DISPLAY_VARIANT := caf
-TARGET_QCOM_MEDIA_VARIANT := caf
 TARGET_USES_QCOM_BSP := true
 
 # Audio
 BOARD_USES_ALSA_AUDIO := true
 BOARD_USES_LEGACY_ALSA_AUDIO := true
-TARGET_USES_QCOM_MM_AUDIO := true
-
+TARGET_USES_QCOM_COMPRESSED_AUDIO := true
 BOARD_HAVE_NEW_QCOM_CSDCLIENT := true
-BOARD_HAVE_CSD_FAST_CALL_SWITCH :=true
-BOARD_USES_FLUENCE_INCALL := true
-BOARD_USES_SEPERATED_AUDIO_INPUT := true
-BOARD_USES_SEPERATED_VOICE_SPEAKER_MIC := true
-
-# QCOM enhanced A/V
-TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
-
-# Legacy RIL
-BOARD_HAS_RIL_LEGACY_PAP := true
+# Not implemented yet
+QCOM_ANC_HEADSET_ENABLED := false
 
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 26
@@ -142,6 +131,9 @@ BOARD_BOOTIMAGE_PARTITION_SIZE := 0x01400000
 BOARD_RECOVERYIMAGE_PARTITION_SIZE := 0x01400000
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1056964608
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 2147483648
+
+# Include common SE policies
+-include device/qcom/sepolicy/sepolicy.mk
 
 BOARD_SEPOLICY_DIRS += \
     device/sony/huashan/sepolicy
@@ -175,3 +167,5 @@ BOARD_SEPOLICY_UNION += \
     vold.te \
     wpa_supplicant.te
 
+# inherit from the proprietary version
+-include vendor/sony/huashan/BoardConfigVendor.mk
