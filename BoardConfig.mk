25f0aa66f1d188b0cd2dd1ab68801b3f18ecc585
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-01-28 16:26:26 +1030

    huashan: cleanup boardconfig.mk
    Change-Id: Ieab52472bfdcaef4f5c9cc73c9c19c31f7838321

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2013 The CyanogenMod Project
+# Copyright (C) 2015 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,12 +18,6 @@ include device/sony/common/BoardConfigCommon.mk
 # inherit from msm8960-common
 include device/sony/msm8960-common/BoardConfigCommon.mk
 
-# inherit from the proprietary version
--include vendor/sony/huashan/BoardConfigVendor.mk
-
-# Assert
-TARGET_OTA_ASSERT_DEVICE := C5302,C5303,C5306,huashan
-
 TARGET_SPECIFIC_HEADER_PATH += device/sony/huashan/include
 
 # Kernel properties
@@ -36,12 +30,10 @@ TARGET_BOARD_PLATFORM := msm8960
 BOARD_VENDOR_PLATFORM := viskan
 
 # Architecture
-TARGET_CPU_VARIANT := krait
-
-# Flags
 TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
+TARGET_CPU_VARIANT := krait
 
 # Krait optimizations
 TARGET_USE_KRAIT_BIONIC_OPTIMIZATION := true
@@ -52,15 +44,18 @@ TARGET_KRAIT_BIONIC_BBTHRESH  := 64
 TARGET_KRAIT_BIONIC_PLDSIZE   := 64
 
 # Kernel information
-BOARD_KERNEL_BASE     := 0x80200000
+BOARD_KERNEL_BASE  := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
 BOARD_KERNEL_CMDLINE  := androidboot.hardware=qcom user_debug=31 androidboot.baseband=msm msm_rtb.filter=0x3F ehci-hcd.park=3 vmalloc=400M
-BOARD_MKBOOTIMG_ARGS  := --ramdisk_offset 0x02000000
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
+
+# Time
+BOARD_USES_QC_TIME_SERVICES := true
 
 # Dumpstate
 BOARD_LIB_DUMPSTATE := libdumpstate.sony
 
-# Wifi Config
+# Wifi
 BOARD_HAS_QCOM_WLAN              := true
 BOARD_WLAN_DEVICE                := qcwcn
 WPA_SUPPLICANT_VERSION           := VER_0_8_X
@@ -85,16 +80,15 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := $(TARGET_BOARD_PLATFORM)
 TARGET_NO_RPC := true
 
 # Bluetooth
-BOARD_HAVE_BLUETOOTH := true
 BOARD_HAVE_BLUETOOTH_QCOM := true
 BLUETOOTH_HCI_USE_MCT := true
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/sony/huashan/bluetooth
 
+# Legacy RIL
+BOARD_HAS_RIL_LEGACY_PAP := true
+
 # Needed for blobs
 COMMON_GLOBAL_CFLAGS += -DNEEDS_VECTORIMPL_SYMBOLS
-
-# Time
-BOARD_USES_QC_TIME_SERVICES := true
 
 # Vold
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
@@ -110,6 +104,9 @@ RECOVERY_FSTAB_VERSION := 2
 BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_HAS_NO_SELECT_BUTTON := true
 TARGET_USERIMAGES_USE_EXT4 := true
+
+# Assert
+TARGET_OTA_ASSERT_DEVICE := C5302,C5303,C5306,huashan
 
 # QCOM/CAF hardware
 BOARD_USES_QCOM_HARDWARE := true
@@ -132,8 +129,6 @@ BOARD_USES_SEPERATED_VOICE_SPEAKER_MIC := true
 # QCOM enhanced A/V
 TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
 
-# Legacy RIL
-BOARD_HAS_RIL_LEGACY_PAP := true
 
 # Partition information
 BOARD_VOLD_MAX_PARTITIONS := 26
@@ -175,3 +170,5 @@ BOARD_SEPOLICY_UNION += \
     vold.te \
     wpa_supplicant.te
 
+# inherit from the proprietary version
+-include vendor/sony/huashan/BoardConfigVendor.mk
