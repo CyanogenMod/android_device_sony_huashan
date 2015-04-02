65da775a7925f4dfef14866a971390c299673eec
Furrydaus <firdaus1999.amin@gmail.com>
2015-04-02 21:34:18 +0800

    Update custombootimg.mk
    CWM Inclusion Patch #2

--- custombootimg.mk
+++ custombootimg.mk
@@ -29,6 +29,8 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel $(uncompressed_ramdisk) $(r
 	$(hide) $(MKBOOTFS) $(PRODUCT_OUT)/combinedroot/ > $(PRODUCT_OUT)/combinedroot.cpio
 	$(hide) cat $(PRODUCT_OUT)/combinedroot.cpio | gzip > $(PRODUCT_OUT)/combinedroot.fs
 	$(hide) python $(MKELF) -o $@ $(PRODUCT_OUT)/kernel@0x80208000 $(PRODUCT_OUT)/combinedroot.fs@0x81900000,ramdisk vendor/sony/huashan/proprietary/boot/RPM.bin@0x00020000,rpm device/sony/huashan/rootdir/cmdline.txt@cmdline
+	$(hide) cd $(PRODUCT_OUT)/recovery && tar -cvf cwm.tar `ls $(PRODUCT_OUT)/recovery`
+	$(hide) mv $(PRODUCT_OUT)/recovery/cwm.tar $(PRODUCT_OUT)/system/bin/cwm.tar
 
 	$(hide) ln -f $(INSTALLED_BOOTIMAGE_TARGET) $(PRODUCT_OUT)/boot.elf
 
