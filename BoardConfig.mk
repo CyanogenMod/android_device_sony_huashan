b773b77abc31f7de8bf181f060a56bb81094f921
Brinly Taylor <uberlaggydarwin@gmail.com>
2015-04-15 20:04:16 +0930

    huashan: Enable SELinux properly
    Needs one more look at the policies.
    
    -2'ing for now.
    
    Change-Id: I96272bba243192c13aae9e44994975dc210e0e9c

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,7 +28,7 @@ BOARD_VENDOR_PLATFORM := viskan
 # Kernel information
 BOARD_KERNEL_BASE  := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE  := androidboot.hardware=qcom user_debug=31 androidboot.baseband=msm msm_rtb.filter=0x3F ehci-hcd.park=3 vmalloc=400M androidboot.selinux=permissive selinux=0
+BOARD_KERNEL_CMDLINE  := androidboot.hardware=qcom user_debug=31 androidboot.baseband=msm msm_rtb.filter=0x3F ehci-hcd.park=3 vmalloc=400M
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 
 # Use legacy MMAP for pre-lollipop blobs
