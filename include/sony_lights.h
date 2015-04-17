a39d006045c842716e0f37a8efd9b9923eaf40f9
FirdausAmin <furrydaus@gmail.com>
2015-04-18 06:59:20 +0800

    huashan:reduce brightness to 1
    Change-Id: Ifcaab68f5b3935668923e628d4358f9ff5e254f6

--- include/sony_lights.h
+++ include/sony_lights.h
@@ -17,4 +17,4 @@
 char const*const LCD_BACKLIGHT_FILE  = "/sys/devices/i2c-10/10-0040/leds/lcd-backlight1/brightness";
 char const*const LCD_BACKLIGHT2_FILE = "/sys/devices/i2c-10/10-0040/leds/lcd-backlight2/brightness";
 char const*const MAX_BRIGHTNESS_FILE = "/sys/devices/i2c-10/10-0040/leds/lcd-backlight1/max_brightness";
-const int LCD_BRIGHTNESS_MIN         = 10;
+const int LCD_BRIGHTNESS_MIN         = 1;
