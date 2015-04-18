d9c9aa9ba2fa4bf054405ea1edc9fce91330e168
Mrcl1450 <marcelo-fil@outlook.com>
2015-04-18 11:14:34 +0100

    huashan: Update audio
    Change-Id: I01bc256e0a0757d520077e47851d0e79a3fd1012

--- huashan.mk
+++ huashan.mk
@@ -200,11 +200,15 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
+    qcom.audio.init=complete \
     persist.audio.fluence.mode=endfire \
+    persist.audio.vr.enable=false \
     persist.audio.handset.mic=digital \
     persist.audio.lowlatency.rec=false \
-    af.resampler.quality=255 \
-    ro.qc.sdk.audio.fluencetype=fluence
+    media.aac_51_output_enabled=true \
+    ro.qc.sdk.audio.ssr=false \
+    ro.qc.sdk.audio.fluencetype=none \
+    lpa.decode=true
 
 # QCOM Location
 PRODUCT_PROPERTY_OVERRIDES += \
