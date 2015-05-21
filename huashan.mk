50ccbfd3e9c2b4804d4342996078e6a2c6c6420d
Mrcl1450 <marcelo-fil@outlook.com>
2015-05-21 20:05:37 +0100

    huashan: Remove density from PRODUCT_AAPT_CONFIG
    AAPT ignores densities in PRODUCT_AAPT_CONFIG.
    The use of PRODUCT_AAPT_PREF_CONFIG for density is encouraged, as AAPT is able to determine the fallback density to use if a resource of the specified density does not exist.
    
    Change-Id: Idf6ebab410e0c2194eec72b2aac06b14963ce392

--- huashan.mk
+++ huashan.mk
@@ -17,11 +17,7 @@ $(call inherit-product, device/sony/msm8960-common/msm8960.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/sony/huashan/overlay
 
-# This device is xhdpi.  However the platform doesn't
-# currently contain all of the bitmaps at xhdpi density so
-# we do this little trick to fall back to the hdpi version
-# if the xhdpi doesn't exist.
-PRODUCT_AAPT_CONFIG := normal hdpi xhdpi
+PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
 
 # Permissions
