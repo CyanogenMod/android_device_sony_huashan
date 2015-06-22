dd926621d64f21ca4737916c3a418155f39a23f2
Ronghua Wu <ronghuawu@google.com>
2015-06-22 03:40:04 +0000

    huashan: add media_codecs_performance.xml
    Change-Id: I5f4ec617d7801664d82629c2fbcba9aacdddb2c0

--- huashan.mk
+++ huashan.mk
@@ -74,7 +74,8 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
     $(LOCAL_PATH)/rootdir/system/etc/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/rootdir/system/etc/media_profiles.xml:system/etc/media_profiles.xml
+    $(LOCAL_PATH)/rootdir/system/etc/media_profiles.xml:system/etc/media_profiles.xml \
+    $(LOCAL_PATH)/rootdir/system/etc/media_codecs_performance.xml:system/etc/media_codecs_performance.xml
 
 # GPS
 PRODUCT_COPY_FILES += \
