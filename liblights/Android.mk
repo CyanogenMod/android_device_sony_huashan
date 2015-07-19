89108a265b2217b5d90555b5bad626909df69591
AdrianDC <radian.dc@gmail.com>
2015-07-19 22:31:39 +0200

    LibLights: Full Illumination Bar implementation
    Source coded library to control the AS3677 and AS3665 components
    for the LCD backlight and LEDs notifications / battery indicator.
    
    Change-Id: Ibcf9e4d3d022add3f43f28984d2e4463ee25ab9b
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

---
+++ liblights/Android.mk
@@ -0,0 +1,29 @@
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+
+ifeq ($(DEVICE_OVERRIDES_LIBLIGHTS),true)
+
+ifneq ($(strip $(TARGET_DISPLAY_GAMMA_DISABLED)),true)
+    LOCAL_CFLAGS += -DENABLE_GAMMA_CORRECTION
+endif
+
+ifneq ($(BOARD_LIGHT_LEDS_COUNT),)
+    LOCAL_CFLAGS := -DLIGHT_LEDS_COUNT=$(BOARD_LIGHT_LEDS_COUNT)
+else
+    LOCAL_CFLAGS := -DLIGHT_LEDS_COUNT=1
+endif
+
+LOCAL_SRC_FILES := \
+    lights.cpp
+
+LOCAL_SHARED_LIBRARIES := \
+    libhardware liblog libutils libcutils
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE := lights.$(TARGET_BOARD_PLATFORM)
+
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)
+
+endif
