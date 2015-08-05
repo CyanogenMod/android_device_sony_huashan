00b33519c8cf048bfc247def813caae92fd0f576
Ziyan <jaraidaniel@gmail.com>
2015-08-05 23:57:00 +0100

    huashan: treat as as low-ram via build.prop
    Change-Id: I8c14a9c0408c2ae9d7cb9986ddf28f79114ba54d

--- huashan.mk
+++ huashan.mk
@@ -243,6 +243,15 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     thermanager
 
+# Low-RAM optimizations
+ADDITIONAL_BUILD_PROPERTIES += \
+    ro.config.low_ram=true \
+    persist.sys.force_highendgfx=true \
+    dalvik.vm.jit.codecachesize=0 \
+    config.disable_atlas=true \
+    ro.config.max_starting_bg=8 \
+    ro.sys.fw.bg_apps_limit=16
+
 # Set default USB interface
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
