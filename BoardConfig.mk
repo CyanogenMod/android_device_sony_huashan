5dba75f22b71e909d5b7dadd1e59d501d18548d8
Mrcl1450 <marcelo-fil@outlook.com>
2015-08-27 02:45:58 +0100

    huashan: move performance items to init.qcom.power.rc
     - Use init.qcom.power.rc for performance items
     - Clean device tree a bit
     - Set bfq as default io
     - Fix some services
    
    Change-Id: I310cbe5c33d26a7199d3e62392c5586e31125c1c
    Signed-off-by: Mrcl1450 <marcelo-fil@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,7 +36,7 @@ BOARD_VENDOR_PLATFORM := viskan
 # Kernel information
 BOARD_KERNEL_BASE  := 0x80200000
 BOARD_KERNEL_PAGESIZE := 2048
-BOARD_KERNEL_CMDLINE  := androidboot.hardware=qcom user_debug=31 androidboot.baseband=msm msm_rtb.filter=0x3F ehci-hcd.park=3 vmalloc=400M
+BOARD_KERNEL_CMDLINE  := androidboot.hardware=qcom androidboot.baseband=msm user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 vmalloc=340M
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x02000000
 
 TARGET_NO_BOOTLOADER := true
