a53bbdfed4321c2f3ee1d6d1fd7f7a8ef27dcb73
Mrcl1450 <marcelo-fil@outlook.com>
2015-09-10 19:48:23 +0100

    huashan: Update media codecs
     From caf LA.AF.1.1_rb1.18
    
    Change-Id: Ifcb2fc79bdca2b99a79bd3d8c3889088e47de5b1
    Signed-off-by: Mrcl1450 <marcelo-fil@outlook.com>

--- huashan.mk
+++ huashan.mk
@@ -69,10 +69,6 @@ PRODUCT_COPY_FILES += \
 
 # Media
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/system/etc/media_profiles.xml:system/etc/media_profiles.xml
-
-# Media
-PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
