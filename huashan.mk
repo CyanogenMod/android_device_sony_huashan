b51068ecdfb3d1836afdfac334c32aef54ded0e2
Mrcl1450 <marcelo-fil@outlook.com>
2015-10-30 14:50:54 +0000

    huashan: M bringup
    Change-Id: I7985347847f3e923cbdea0b4d74dd636c27e1035
    Signed-off-by: Mrcl1450 <marcelo-fil@outlook.com>

--- huashan.mk
+++ huashan.mk
@@ -182,11 +182,11 @@ PRODUCT_PACKAGES += \
     tinymix
 
 # FM radio
-PRODUCT_PACKAGES += \
-    FM2 \
-    FMRecord \
-    libqcomfm_jni \
-    qcom.fmradio
+#PRODUCT_PACKAGES += \
+#    FM2 \
+#    FMRecord \
+#    libqcomfm_jni \
+#    qcom.fmradio
 
 # Bluetooth
 PRODUCT_PACKAGES += \
@@ -226,11 +226,6 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
-# Misc
-PRODUCT_PACKAGES += \
-    librs_jni \
-    com.android.future.usb.accessory
-
 # Lights
 PRODUCT_PACKAGES += \
     lights.msm8960
@@ -250,10 +245,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 # Dalvik
 PRODUCT_PROPERTY_OVERRIDES += \
     dalvik.vm.dex2oat-swap=false
-
-# USB OTG support
-PRODUCT_PROPERTY_OVERRIDES += \
-    persist.sys.isUsbOtgEnabled=true
 
 # Radio and Telephony
 PRODUCT_PROPERTY_OVERRIDES += \
