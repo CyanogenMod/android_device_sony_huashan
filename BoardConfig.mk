d4d25b532a04b58c80f4d0dd929b27cdc43491dd
Steve Kondik <steve@cyngn.com>
2015-11-07 06:02:20 -0800

    huashan: Build the kernel with gcc-4.9
    Change-Id: Ia0c0cc45b2c192ad7262d797ee3c469b94644194

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,6 +25,7 @@ TARGET_CPU_VARIANT := krait
 # Kernel properties
 TARGET_KERNEL_SOURCE := kernel/sony/msm8960t
 TARGET_KERNEL_CONFIG := cm_viskan_huashan_defconfig
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := arm-linux-androideabi-
 
 # Platform
 BOARD_VENDOR := sony
