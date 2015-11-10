5fd61228c92c33d60d86da5a40d87ee4fc2b0051
AdrianDC <radian.dc@gmail.com>
2015-11-10 01:58:23 +0100

    huashan: Provide a camera SHIM library for SONY libs
     * Based upon 5.1 frameworks/native sources
     * Updated with the related 6.0 source updates
        without breakage and to avoid all warnings
     * Provide additional symbols for compatibility
     * Only SHIM against camera.vendor.qcom.so
    
     * Thanks to :
      - Christopher R. Palmer for the "Properly shim VectorImpl" and
         SensorManager fixes from "Shim the camera hal to get it loading"
      - Otto Juntunen for pointing out the Looper.cpp requirement
    
    Change-Id: Idd03ff4d7226108024e734c002102b47d7dbeace
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

--- libshims/Android.mk
+++ libshims/Android.mk
@@ -1,4 +1,4 @@
-# Copyright (C) 2015 The CyanogenMod Project
+# Copyright (C) 2015-2016 The CyanogenMod Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,6 +14,39 @@
 
 LOCAL_PATH := $(call my-dir)
 
+# libshim_camera
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+    gui/SensorManager.cpp \
+    ui/GraphicBuffer.cpp \
+    ui/GraphicBufferAllocator.cpp \
+    ui/GraphicBufferMapper.cpp \
+    utils/Looper.cpp \
+    utils/VectorImpl.cpp \
+    symbols/Parcel.cpp
+
+LOCAL_C_INCLUDES := \
+    $(LOCAL_PATH)/include \
+    external/safe-iop/include
+
+LOCAL_SHARED_LIBRARIES := \
+    liblog \
+    libcutils \
+    libhardware \
+    libui \
+    libgui \
+    libbinder \
+    libutils \
+    libsync
+
+LOCAL_MODULE := libshim_camera
+
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE_CLASS := SHARED_LIBRARIES
+
+include $(BUILD_SHARED_LIBRARY)
+
 # libshim_qcopt
 include $(CLEAR_VARS)
 
@@ -23,6 +56,9 @@ LOCAL_SRC_FILES := \
 LOCAL_SHARED_LIBRARIES := \
     libicuuc \
     libicui18n
+
+LOCAL_CFLAGS += \
+    -Wall
 
 LOCAL_MODULE := libshim_qcopt
 
