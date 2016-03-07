b6afffaaaacd67f333a967518255325aa4a8931c
AdrianDC <radian.dc@gmail.com>
2016-03-07 19:54:10 +0100

    huashan: Add Configs and bootlogo removed from device_sony_common
    Change-Id: Ic2ba69d2e79b6220180bcb5ae90e33c4f595d123
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

--- full_huashan.mk
+++ full_huashan.mk
@@ -28,9 +28,6 @@ TARGET_SCREEN_WIDTH := 720
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Common Sony Resources
-$(call inherit-product, device/sony/common/resources.mk)
-
 # Inherit from huashan device makefile
 $(call inherit-product, device/sony/huashan/huashan.mk)
 
