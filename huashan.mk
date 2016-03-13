ea2c090619b137fef3dd538250797cc2393ceef0
AdrianDC <radian.dc@gmail.com>
2016-03-13 19:54:42 +0100

    huashan: Switch to the new msm8960 audio HAL
     * Reupdated thanks to Arne Coucheron :
       * Change-Id I3b180427743f56b384a72f948948a357ae7310d4
       * Deprecate old defines
       * Deprecate old system props and use new ones
       * Change audio_policy.conf for new HAL
       * Remove old sound use case file
    
     * Custom audio_platform_info.xml based on legacy values
     * Implement custom mixer_paths based on legacy hal config
     * Improvements on some audio configurations and volumes
     * Restored support for audio over MHL HDMI
    
    Change-Id: I88ce0a3ae09619370d92e2deb804c0ff95378fff
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

--- huashan.mk
+++ huashan.mk
@@ -74,8 +74,10 @@ PRODUCT_COPY_FILES += \
 
 # Audio
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/system/etc/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/rootdir/system/etc/snd_soc_msm/snd_soc_msm_2x:system/etc/snd_soc_msm/snd_soc_msm_2x
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
 
 # Media
 PRODUCT_COPY_FILES += \
@@ -184,11 +186,12 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PACKAGES += \
-    audio.primary.msm8960 \
     audio.a2dp.default \
-    audio.usb.default \
+    audio.primary.msm8960 \
     audio.r_submix.default \
+    audio.usb.default \
     libaudio-resampler \
+    libqcomvoiceprocessing \
     tinymix
 
 # FM radio
@@ -286,11 +289,13 @@ PRODUCT_PACKAGES += \
 
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
-    persist.audio.fluence.mode=endfire \
-    persist.audio.handset.mic=digital \
-    persist.audio.lowlatency.rec=false \
-    af.resampler.quality=255 \
-    ro.qc.sdk.audio.fluencetype=fluence
+    audio.offload.disable=1 \
+    mm.enable.smoothstreaming=true \
+    ro.qc.sdk.audio.fluencetype=none \
+    persist.audio.fluence.voicecall=true \
+    persist.audio.fluence.voicerec=false \
+    persist.audio.fluence.speaker=true \
+    qcom.hw.aac.encoder=true
 
 # QCOM
 PRODUCT_PROPERTY_OVERRIDES += \
