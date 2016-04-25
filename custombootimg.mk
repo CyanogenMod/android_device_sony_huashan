709eb1197edb12b6196359bac678fd82f18341dd
AdrianDC <radian.dc@gmail.com>
2016-04-25 23:31:30 +0200

    huashan: Add assert-max-image-size for boot image rule
     * Thanks to Matt Mower for pointing out the absence of
        this partition size check in all sources since the beginning
    
    Change-Id: I22dc3f100679e6c58af6e7f9952ea9040250771b

--- custombootimg.mk
+++ custombootimg.mk
@@ -60,6 +60,7 @@ $(INSTALLED_BOOTIMAGE_TARGET): $(PRODUCT_OUT)/kernel \
 	$(hide) python $(MKELF) -o $@ $(PRODUCT_OUT)/kernel@0x80208000 $(PRODUCT_OUT)/combinedroot.fs@0x81900000,ramdisk vendor/sony/huashan/proprietary/boot/RPM.bin@0x00020000,rpm device/sony/huashan/rootdir/cmdline.txt@cmdline
 
 	$(hide) ln -f $(INSTALLED_BOOTIMAGE_TARGET) $(PRODUCT_OUT)/boot.elf
+	$(hide) $(call assert-max-image-size,$@,$(BOARD_BOOTIMAGE_PARTITION_SIZE))
 	$(call pretty,"Made boot image: $@")
 
 INSTALLED_RECOVERYIMAGE_TARGET := $(PRODUCT_OUT)/recovery.img
