82724d8589de5f426904db5c884de2aeaf15d4e4
AdrianDC <radian.dc@gmail.com>
2016-05-07 02:49:55 +0200

    huashan: Improve Camera HAL wrapper for SensorService init
     * Update based upon original commit in oppo_msm8939-common
        I1fcf77146317c31407a0efc16429066555186643 by Danny Baumann
    
    Change-Id: I159c390f24e553cc738b9cd407f49cc1d9c23a78

--- camera/Android.mk
+++ camera/Android.mk
@@ -10,11 +10,10 @@ LOCAL_C_INCLUDES := \
     system/media/camera/include
 
 LOCAL_SHARED_LIBRARIES := \
-    libhardware liblog libcamera_client libutils
+    libhardware liblog libcamera_client libgui libutils
 
-LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE_RELATIVE_PATH := hw
 LOCAL_MODULE := camera.qcom
-
 LOCAL_MODULE_TAGS := optional
 
 include $(BUILD_SHARED_LIBRARY)
