6debbc90b68fc208e8370885f2a01bdb93189082
AdrianDC <radian.dc@gmail.com>
2016-05-29 11:26:02 +0200

    huashan: Enable ADB insecure for test builds
     * Do not merge in any release
    
    Change-Id: I3d80e59678afae71e8391a79456c5359f0b66063

--- product/device.mk
+++ product/device.mk
@@ -1,3 +1,9 @@
 # Glove mode
 PRODUCT_PACKAGES += \
     DeviceSettings
+
+# ADB properties
+ADDITIONAL_DEFAULT_PROPERTIES += \
+    persist.service.adb.enable=1 \
+    ro.adb.secure=0 \
+    ro.secure=0
