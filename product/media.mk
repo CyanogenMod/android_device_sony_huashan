37b11b920b3db39037f6678ed0218ea8750aaad4
AdrianDC <radian.dc@gmail.com>
2016-05-29 16:57:47 +0200

    huashan: Cleanup unneeded product/media.mk line-break
     * Pointed out by Chippa_a
    
    Change-Id: I7b1aaeab0d0685dcd3ec1dd98f93f52e76fa5f1f

--- product/media.mk
+++ product/media.mk
@@ -2,7 +2,7 @@
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
 # Media configurations
 PRODUCT_COPY_FILES += \
