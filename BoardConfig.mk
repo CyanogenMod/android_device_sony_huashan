3d61772aef70c68244b5351fb048138253fa3806
AdrianDC <radian.dc@gmail.com>
2016-06-16 22:12:42 +0200

    huashan: Import arch configs first from a PlatformConfig.mk
     * Preserve the primary config flag TARGET_BOARD_PLATFORM
        as the first flags included by the BoardConfig
        and avoid using a numerical name for the file
    
     * Address the Android repo-wide issue of being unable
        to compile on an Ubuntu 16 computer where the 00-*
        files are apparently not being loaded first
    
    Change-Id: I7833730253eb4402aca3d418b8f9036985343e9f
    Signed-off-by: AdrianDC <radian.dc@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -21,6 +21,7 @@ DEVICE_PATH := device/sony/huashan
 TARGET_SPECIFIC_HEADER_PATH := $(DEVICE_PATH)/include
 
 # Device board elements
+include $(DEVICE_PATH)/PlatformConfig.mk
 include $(DEVICE_PATH)/board/*.mk
 
 # Device vendor board
