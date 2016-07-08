188201fda5b3262e5504bce3ddc217858434a595
RonGokhale <cips173@gmail.com>
2016-07-08 19:24:11 +0200

    huashan: nfc: Delete user nfcee_access.xml configuration
     * As the device will never be built in user mode,
        delete this only reference to user aimed builds
    
    Change-Id: I4fd1c8c2e5b2c226e340fa72a3bc99d1fee8d608

--- product/nfc.mk
+++ product/nfc.mk
@@ -1,10 +1,3 @@
-# NFCEE access selection
-ifeq ($(TARGET_BUILD_VARIANT),user)
-    NFCEE_ACCESS_PATH := $(LOCAL_PATH)/nfc/nfcee_access.xml
-else
-    NFCEE_ACCESS_PATH := $(LOCAL_PATH)/nfc/nfcee_access_debug.xml
-endif
-
 # NFCEE access configuration
 PRODUCT_COPY_FILES += \
-    $(NFCEE_ACCESS_PATH):system/etc/nfcee_access.xml
+    $(LOCAL_PATH)/nfc/nfcee_access_debug.xml:system/etc/nfcee_access.xml
