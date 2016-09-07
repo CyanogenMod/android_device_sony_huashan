9c91c3bf78378f57b7e3722e822aa291484d6f53
Adrian DC <radian.dc@gmail.com>
2016-09-07 16:09:53 +0200

    huashan: cm: Switch to minimal configuration and keep vendor apps
     * Stop importing full config with dictionnaries,
        theme and other additional elements
    
     * Preserve the compilation of LiveWallpapersPicker,
        SoundRecorder and Screencast as they are both
        lightweight and useful to most users
    
    Change-Id: Ieb042138c0d325fb40d4f1042d8bb8b9ce19cfc8

---
+++ product/vendor.mk
@@ -0,0 +1,19 @@
+# Vendor packages
+PRODUCT_PACKAGES += \
+    LiveWallpapersPicker \
+    SoundRecorder \
+    Screencast
+
+# Extra tools in CM
+PRODUCT_PACKAGES += \
+    7z \
+    lib7z \
+    bash \
+    bzip2 \
+    curl \
+    powertop \
+    unrar \
+    unzip \
+    vim \
+    wget \
+    zip
